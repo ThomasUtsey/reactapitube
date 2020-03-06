d3d80900fbcf6759e3ef5b07910b462b3284a9ee
Thomas Utsey <tom@edgegluer.com>
2020-03-06 17:00:24 -0500

    Updates

---
+++ precache-manifest.b8ed7b35d6b84b57677415d973cfa4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0b3c8fa1ce99d83172b9ee12945ed4",
+    "url": "/reactapitube/index.html"
+  },
+  {
+    "revision": "ffec7c3e678df5221051",
+    "url": "/reactapitube/static/css/main.3fc4b0ce.chunk.css"
+  },
+  {
+    "revision": "f7e15fd87c38d3c1bdae",
+    "url": "/reactapitube/static/js/2.95fc45c4.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/reactapitube/static/js/2.95fc45c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffec7c3e678df5221051",
+    "url": "/reactapitube/static/js/main.5f5a7d02.chunk.js"
+  },
+  {
+    "revision": "d1c683b47e98141de341",
+    "url": "/reactapitube/static/js/runtime-main.17d8b0c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c66d7567f6b80cea46340a1e962173e.js"
+  "/reactapitube/precache-manifest.b8ed7b35d6b84b57677415d973cfa4b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactapitube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
